777a8ade79b7256247fa90407ca87c3ccb9a4e1e
SKE0653 <39362991+SKE0653@users.noreply.github.com>
2025-08-17 14:58:56 +0900

    Update 조인.sql

--- 2과목/조인.sql
+++ 2과목/조인.sql
@@ -1,6 +1,14 @@
 
-left outer join
-- from table a left outer join table b
-on a.col= b.col 이것과 같은 오라클 5g 문법은
-- from table a, table b
-where a.col = b.col(+)
+-- left outer join
+  select  *
+  from              A
+  left outer join   B
+  on  A.col = B.col ;
+ -- 이것과 같은 오라클 5g 문법은
+  select  *
+  from    A, B
+  where   A.col = B.col(+) ;
+
+
+-- inner join
+
